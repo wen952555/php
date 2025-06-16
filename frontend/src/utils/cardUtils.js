b8c694a9fd2477a8eea1920ae87b6cc9f0e25f06
wen952555 <wenxiu9525@outlook.com>
2025-06-16 13:45:36 +0800

    Delete frontend directory

--- frontend/src/utils/cardUtils.js
+++
@@ -1,9 +0,0 @@
-export function getCardImage(card) {
-  const valueMap = { 'A': 'ace', 'J': 'jack', 'Q': 'queen', 'K': 'king' };
-  const suitMap = { 'spades': 'spades', 'hearts': 'hearts', 'diamonds': 'diamonds', 'clubs': 'clubs' };
-  let value = valueMap[card.value] || card.value;
-  return `/cards/${value}_of_${suitMap[card.suit]}.svg`;
-}
-
-export const SUITS = ['spades', 'hearts', 'diamonds', 'clubs'];
-export const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
